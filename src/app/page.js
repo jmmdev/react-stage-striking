07077f2ff330d275509978f3869717e3346eace8
José María Martín Muñoz <josemaria.martin169@gmail.com>
2024-01-19 02:57:56 +0100

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -7,25 +7,25 @@ import styles from './page.module.css'
 import Stages from './components/stages'
 import StageSettings from './components/stage-settings';
 
-export const getStaticProps = async () => {
-    const res = await fetch('https://files/stages.json')
+export const getStages = async () => {
+    const res = await fetch('https://files/stages.json', {cache: 'force-cache'})
     const stage_data = await res.json() 
 
-    return {
-      stage_data: {
-        stage_data,
-      },
-    }
+    return stage_data
 }
 
-export default function Home({stage_data}) {
+export default function Home() {
   const [stages, setStages] = useState([])
   const [banned, setBanned] = useState([])
   const [active, setActive] = useState([0,1,2,3,4,5,6,7,10])  
   const [showSettings, setShowSettings] = useState(false)
 
   useEffect(() => {
-    setStages(stages_data)
+    const initialize = async () => {
+      const stage_data = await getStages()
+      setStages(stage_data)
+    }
+    initialize()
   }, [])
 
   const Settings = () => {
